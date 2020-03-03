f59ab2da3b6da2ae7f245f3c2e6bd54564e0a750
nuswen <donn.nus@gmail.com>
2020-03-03 17:58:06 +0300

    init

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from app import db
-from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.dialects.postgresql import JSONB
 
 
 class story(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.Text)
-    answers = db.Column(db.Jsonb)
+    answers = db.Column(JSONB)
     link = db.Column(db.Integer)
     timeout = db.Column(db.Integer)
     branch = db.Column(db.Text)
     photo = db.Column(db.Text)
     audio = db.Column(db.Text)
-    speclink = db.Column(db.Jsonb)
+    speclink = db.Column(JSONB)
