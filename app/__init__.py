ee0ec549837564c338a64f10ca16b5977086418d
nuswen <donn.nus@gmail.com>
2020-03-17 09:56:39 +0300

    thred

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ from app import tele_bot, models
 def activate_job():
     def run_job():
         while True:
-            print("Run recurring task")
+            poster(bot, 2601798, text='hi')
             time.sleep(3)
 
     thread = threading.Thread(target=run_job)
