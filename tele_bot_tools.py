05d726e651aedd92bb0062423a4bffa1e86a5d8e
nuswen <donn.nus@gmail.com>
2020-04-24 15:17:31 +0300

    init

--- tele_bot_tools.py
+++ tele_bot_tools.py
@@ -45,7 +45,7 @@ def inlineKeyboarder(rows):
             keysRows[rowCount].append(types.InlineKeyboardButton(text=key, callback_data=json.dumps(row[key])))
 
     for row in keysRows:
-        markup.row_width = len(row)
+        keyboard.row_width = len(row)
         keyboard.add(row)
         break
     return keyboard
