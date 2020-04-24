2442780f15296215748510d8bc65332f84749684
nuswen <donn.nus@gmail.com>
2020-04-24 14:22:41 +0300

    init

--- tele_bot_tools.py
+++ tele_bot_tools.py
@@ -34,17 +34,14 @@ def poster(bot, chatId, text=None, buttons=None, ed=False, message_id=None, doc=
 
 def inlineKeyboarder(rows):
     rows = [{'hi':{'show':'ttt'},'buy':{'show':'ttt'},'lo':{'show':'ttt'}},{'hi':{'show':'ttt'},'buy':{'show':'ttt'},'lo':{'show':'ttt'}}]
-    #Функция должна собирать клавиатуру для бота, получает keys в виде словаря {'key':'call_back_data',...}
     keysRows = [[],]
     rowCount = 0
 
-    #проходимся по ключам
     for row in rows:
         rowCount = len(keysRows)-1
         for key in row:
             keysRows[rowCount].append(types.InlineKeyboardButton(text=key, callback_data='2'))
         print (keysRows)
-        keysRows.append([])
     
     keyboard = types.InlineKeyboardMarkup(keysRows)
     return keyboard
