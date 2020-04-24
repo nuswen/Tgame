b326543c4dca281da64d8db203ceab7a6ffaade8
nuswen <donn.nus@gmail.com>
2020-04-24 14:41:47 +0300

    init

--- tele_bot_tools.py
+++ tele_bot_tools.py
@@ -48,7 +48,7 @@ def inlineKeyboarder(rows):
     b = types.InlineKeyboardButton(text='key1', callback_data='1')
     c = types.InlineKeyboardButton(text='key2', callback_data='2')
     r = [[a,b],[c]]
-    keyboard = types.InlineKeyboardMarkup(r)
+    keyboard = types.InlineKeyboardMarkup(a,b,c)
     print (keyboard)
     return keyboard
 
