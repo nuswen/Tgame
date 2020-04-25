b5b7524d4986733f31cc415a183e5d6b93719564
nuswen <donn.nus@gmail.com>
2020-04-25 20:20:12 +0300

    init

--- tools.py
+++ tools.py
@@ -30,7 +30,7 @@ def show(userId,commands):
     
 def addWord(userId,commands):
     user = models.telegram_users.query.filter_by(userId = userId).first()
-    user.words.update({commands['word']:0})
+    user.words.update({int(commands['word']):0})
     models.telegram_users.query.filter_by(userId = userId).update({'words': user.words})
     db.session.commit()
     sentence(user,ed=True,startWord=commands['startWord'])
