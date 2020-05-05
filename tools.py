4f6a96e685156c5978aac8be3e09ce930156b846
nuswen <donn.nus@gmail.com>
2020-05-05 10:26:15 +0300

    init

--- tools.py
+++ tools.py
@@ -256,6 +256,6 @@ def ref(userId):
     user = models.telegram_users.query.filter_by(userId = userId).first()
     text = 'Текст сообщения'
     buttonText = 'поделись'
-    shareUrl = 'https://t.me/share/url?url=(https://t.me/{botName}?start={sharePoint})&text=({textForShare})'.format(botName=environ['botName'], sharePoint=str(user.userId),textForShare = 'Гля')
+    shareUrl = 'https://t.me/share/url?url=https://t.me/{botName}?start={sharePoint}&text=({textForShare}'.format(botName=environ['botName'], sharePoint=str(user.userId),textForShare = 'Гля')
     print(shareUrl)
-    poster(bot,userId,text=text,buttons=[{text:shareUrl}],inline=True)
+    poster(bot,userId,text=text,buttons=[{buttonText:shareUrl}],inline=True)
